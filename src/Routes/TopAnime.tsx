83cfe70058d70fab0c32f3d4828f823494f1f3bb
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 17:51:37 +0200

    Feat: Created TopAnime Route

---
+++ src/Routes/TopAnime.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+const TopAnime = () => {
+
+
+    return (
+        <div className='topAnimePage'>
+            <Header></Header>
+        </div>
+    )
+}
+
+export default TopAnime;
