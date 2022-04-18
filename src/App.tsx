3ec4408ed1d35503370453a627e948b0dce6b7de
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 13:43:47 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Home from "./Routes/Home"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Home></Home>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+
+    return (
+        <header>
+            <div className="headerContainer">
+            </div>
+        </header>
+    )
+}
+
+export default Header;
