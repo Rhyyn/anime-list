aba59ca6275551314f06065dcbf987b750c2ea25
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 16:23:18 +0200

    Feat: Created Header with Light/Dark mode toggle

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,28 @@
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Button, Flex, useColorMode } from "@chakra-ui/react";
+
 import React from "react";
+import Navbar from "./NavBar"
 
 const Header = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
 
+    
+    const colorModeIcon = () => { // function used to toggle icons for DARK MODE
+        if (colorMode=== "light") {
+            return <MoonIcon className="colorModeIcon" boxSize={6} marginRight="1rem"  onClick={toggleColorMode}>{colorMode === 'light' ? 'Dark' : 'Light'}</MoonIcon>
+        } else {
+            return <SunIcon className="colorModeIcon" boxSize={6} marginRight="1rem"  onClick={toggleColorMode}>{colorMode === 'dark' ? 'Dark' : 'Light'}</SunIcon>
+        }
+        
+    }
 
     return (
         <header>
-            <div className="headerContainer">
-            </div>
+            <Flex className="headerContainer" bg="purple.400" maxW='100%' color="white" height="60px" alignItems="center" justifyContent="space-between">
+                <Navbar></Navbar>
+                {colorModeIcon()}
+            </Flex>
         </header>
     )
 }
