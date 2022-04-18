1872f8cd8b3ad08cc10f315be4ede5f08b976f8e
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 17:50:54 +0200

    Style: BgColor for DarkTheme

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,12 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { Button, Flex, useColorMode } from "@chakra-ui/react";
+import { Button, Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
 
 import React from "react";
 import Navbar from "./NavBar"
 
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode()
-
+    const bgColor = useColorModeValue('purple.400', 'gray.700') // bg color of the container : light | dark
     
     const colorModeIcon = () => { // function used to toggle icons for DARK MODE
         if (colorMode=== "light") {
@@ -19,7 +19,7 @@ const Header = () => {
 
     return (
         <header>
-            <Flex className="headerContainer" bg="purple.400" maxW='100%' color="white" height="60px" alignItems="center" justifyContent="space-between">
+            <Flex className="headerContainer" bg={bgColor} maxW='100%' color="white" height="60px" alignItems="center" justifyContent="space-between">
                 <Navbar></Navbar>
                 {colorModeIcon()}
             </Flex>
