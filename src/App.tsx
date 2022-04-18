3482e6ef7de8bc5df7b5cb1ecf52efe29597a4f0
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 17:49:14 +0200

    Feat: Fetch from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Home from "./Routes/Home"
+import {BrowserRouter, Routes, Route,} from "react-router-dom";
+import TopAnime from './Routes/TopAnime';
 
 function App() {
+  const [data, setData] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   fetch('https://api.jikan.moe/v4/anime')
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(
+  //           `This is an HTTP error: The status is ${response.status}`
+  //         );
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => setData(data))
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // }, [])
 
   return (
     <div className="App">
-        <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/top" element={<TopAnime />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
