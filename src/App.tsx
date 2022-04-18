036c95557216efb6bc7ed521923f0f770c75f285
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 16:24:36 +0200

    Fix: Clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Home from "./Routes/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
         <Home></Home>
-      </header>
     </div>
   )
 }
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from '../Components/Header';
 
 
 const Home = () => {
 
 
     return (
-        <div>
+        <div className='homePage'>
+            <Header></Header>
             <h3>Hello World</h3>
         </div>
     )
