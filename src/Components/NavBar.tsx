0b50a9c2adafa22e047b1ebd9ecc8d82480b6126
Rhyn <rhyyn.lol@gmail.com>
2022-04-18 17:51:22 +0200

    Feat: Added NavBar Buttons

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
-import { Flex, useColorModeValue, Text } from '@chakra-ui/react'
+import { Flex, useColorModeValue, Text, Button, ButtonGroup } from '@chakra-ui/react'
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { useMediaQuery } from '@chakra-ui/react'
+import { useMediaQuery, Link } from '@chakra-ui/react'
+import { Link as ReachLink } from "react-router-dom";
+
 
 const NavBar = () => {
 
-    const [navBarShrink] = useMediaQuery('(max-width: 480px)')
+    const [navBarShrink] = useMediaQuery('(max-width: 440px)')
+    const buttonBg = useColorModeValue('gray.600', 'purple.700')
+    const buttonBgHover = useColorModeValue('purple.500', 'blue.700')
+    const color = useColorModeValue('white', 'white')
 
 
     return (
         <div className="NavBarMainDiv">
             {navBarShrink ? <HamburgerIcon className="navBarHamburger" boxSize={6} marginLeft="1rem"></HamburgerIcon> : null}
-            {navBarShrink ? null : <Flex className="navBarContainer">
-                <h4 className="navBarItem">Home</h4>
-                <h4 className="navBarItem">Top Anime</h4>
-                <h4 className="navBarItem">Seasonal Anime</h4>
-                <h4 className="navBarItem">Genres</h4>
+            {navBarShrink ? null : <Flex className="navBarContainer" marginLeft="1rem">
+                <ButtonGroup spacing="0.5" alignItems="center" >
+                    <Button padding="0.5rem" fontSize="1rem" bg="none" color={color} _hover={{ bg: buttonBgHover}}><Link as={ReachLink} to="/">Home</Link></Button>
+                    <Button padding="0.5rem" fontSize="1rem" bg="none" color={color} _hover={{ bg: buttonBgHover}}><Link as={ReachLink} to="/top">Top Anime</Link></Button>
+                    <Button padding="0.5rem" fontSize="1rem" bg="none" color={color} _hover={{ bg: buttonBgHover}}><Link as={ReachLink} to="/seasonal">Seasonal Anime</Link></Button>
+                    <Button padding="0.5rem" fontSize="1rem" bg="none" color={color} _hover={{ bg: buttonBgHover}}><Link as={ReachLink} to="/genres">Genres</Link></Button>
+                </ButtonGroup>
             </Flex>}
 
         </div>
