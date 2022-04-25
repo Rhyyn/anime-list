0c4ee7e4f1adb9529e8c0797767d92a230da4a2b
Rhyn <rhyyn.lol@gmail.com>
2022-04-25 12:36:10 +0200

    Feat: Added Card with top anime Data

--- src/Routes/TopAnime.tsx
+++ src/Routes/TopAnime.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Card from '../Components/Card';
 
-const TopAnime = () => {
+const TopAnime = ({data}:{ data:any }) => {
 
-
+    
     return (
         <div className='topAnimePage'>
             <Header></Header>
+            <Card data={data}></Card>
         </div>
     )
 }
